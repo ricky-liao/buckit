63fb09155b457b690da0a78a8f0bb24edbb881d7
carl-searle <carl1298@outlook.com>
2023-01-21 17:30:28 -0800

    commit

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function Home ({navigation}) {
+    const [fontsLoaded] = useFonts({
+        'Roboto-Bold': require('../assets/fonts/Roboto-Bold.ttf'),
+    });
+    
+    const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+    }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <Text style={styles.title}>Hi,</Text>
             <Button
-            title="Go to Login"
-            onPress={() => navigation.navigate('Login')}
+                title="Go to Login"
+                onPress={() => navigation.navigate('Login')}
             />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "blue",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    title: {
+        fontFamily: "Roboto-Regular",
+    },
+});
